7b0f51438e22690b3ece225b54b31911c605ee49
unknown <kanakulyajameson@gmail.com>
2020-07-05 11:26:11 +0400

    Updates

---
+++ precache-manifest.09ad00e42099a0e8626e04501cd39189.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52e3dd5869e9d8a9913fbb3a3d1af67",
+    "url": "/Jamesonkanakulya/personalblog.git/index.html"
+  },
+  {
+    "revision": "21d613273d0f8f68ae98",
+    "url": "/Jamesonkanakulya/personalblog.git/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8e943b339db3581eccdc",
+    "url": "/Jamesonkanakulya/personalblog.git/static/css/main.d9267848.chunk.css"
+  },
+  {
+    "revision": "21d613273d0f8f68ae98",
+    "url": "/Jamesonkanakulya/personalblog.git/static/js/2.105d9206.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Jamesonkanakulya/personalblog.git/static/js/2.105d9206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e943b339db3581eccdc",
+    "url": "/Jamesonkanakulya/personalblog.git/static/js/main.d2bf0a90.chunk.js"
+  },
+  {
+    "revision": "4cb679559dae0472f5e1",
+    "url": "/Jamesonkanakulya/personalblog.git/static/js/runtime-main.9a103a75.js"
+  },
+  {
+    "revision": "f399f7d26714e07573121d52e533335c",
+    "url": "/Jamesonkanakulya/personalblog.git/static/media/background.f399f7d2.jpg"
+  },
+  {
+    "revision": "4981bd760c6646f3a516dc80d058c84d",
+    "url": "/Jamesonkanakulya/personalblog.git/static/media/background2.4981bd76.png"
+  },
+  {
+    "revision": "8d0fe1be6b5dce12e4f2ddb6a0930792",
+    "url": "/Jamesonkanakulya/personalblog.git/static/media/background4.8d0fe1be.jpg"
+  },
+  {
+    "revision": "cf738cb119d712cd46e716a6bfe44bf4",
+    "url": "/Jamesonkanakulya/personalblog.git/static/media/next.cf738cb1.jpeg"
+  },
+  {
+    "revision": "592cad667c168a0011853f4d53779574",
+    "url": "/Jamesonkanakulya/personalblog.git/static/media/top.592cad66.jpeg"
+  }
+]);
